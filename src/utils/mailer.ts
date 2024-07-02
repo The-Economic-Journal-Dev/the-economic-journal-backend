33c03e0eab6bd2d1b7cce1b83bb8ec356fd34311
d8rp <thanh062007gmail.com>
2024-07-03 01:29:48 +0700

    make verifivation emails work

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,18 +1,14 @@
 import nodemailer from "nodemailer";
-import * as aws from "@aws-sdk/client-ses";
-
-const ses = new aws.SES({
-  apiVersion: "2010-12-01",
-  region: process.env.AWS_REGION,
-  credentials: {
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-  },
-});
 
 // Create a transporter object using the default SMTP transport
 const transporter = nodemailer.createTransport({
-  SES: { ses, aws },
+  host: "smtp.zoho.com", // SMTP host
+  port: 465, // SMTP port
+  secure: true, // true for 465, false for other ports
+  auth: {
+    user: "the-economic-journal@zohomail.com",
+    pass: process.env.ZOHO_APP_PASSWORD,
+  },
 });
 
 export default transporter;
