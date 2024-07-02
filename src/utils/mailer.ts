6a1898339323914ec0686fd7c44fa9a72d8964ad
d8rp <thanh062007gmail.com>
2024-07-03 02:58:16 +0700

    make email verification sended

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -3,8 +3,8 @@ import nodemailer from "nodemailer";
 // Create a transporter object using the default SMTP transport
 const transporter = nodemailer.createTransport({
   host: "smtp.zoho.com", // SMTP host
-  port: 465, // SMTP port
-  secure: true, // true for 465, false for other ports
+  port: 587, // SMTP port
+  secure: false, // true for 465, false for other ports
   auth: {
     user: "the-economic-journal@zohomail.com",
     pass: process.env.ZOHO_APP_PASSWORD,
